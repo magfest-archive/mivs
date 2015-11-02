cb41d1f16eb4befce305b7581e789d1dd24c0c3c
Eli Courtwright <eli@courtwright.org>
2015-11-01 19:50:59 -0500

    fixes #8: adds studio page with links for judges

--- mivs/site_sections/mivs_judging.py
+++ mivs/site_sections/mivs_judging.py
@@ -9,6 +9,14 @@ class Root:
             'judge': session.logged_in_judge()
         }
 
+    def studio(self, session, message='', **params):
+        studio = session.indie_studio(params)
+        if cherrypy.request.method == 'POST':
+            # We currently only update notes, though we may add other things to this form later
+            raise HTTPRedirect('index?message={}', 'Notes updated')
+
+        return {'studio': studio}
+
     def video_review(self, session, message='', **params):
         review = session.indie_game_review(params)
         if cherrypy.request.method == 'POST':
