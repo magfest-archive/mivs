eebf522e102e522374845f7f3718978399603e15
Eli Courtwright <eli@courtwright.org>
2015-11-30 22:24:50 -0500

    added judge intro email

--- mivs/automated_emails.py
+++ mivs/automated_emails.py
@@ -2,14 +2,17 @@ from mivs import *
 
 AutomatedEmail.extra_models[IndieStudio] = lambda session: session.query(IndieStudio).all()
 AutomatedEmail.extra_models[IndieGame] = lambda session: session.query(IndieGame).all()
+AutomatedEmail.extra_models[IndieJudge] = lambda session: session.query(IndieJudge).all()
 
 
 class MIVSEmail(AutomatedEmail):
     def __init__(self, *args, **kwargs):
+        if len(args) < 4 and 'filter' not in kwargs:
+            kwargs['filter'] = lambda x: True
         AutomatedEmail.__init__(self, *args, sender=c.MIVS_EMAIL, **kwargs)
 
 
-MIVSEmail(IndieStudio, 'Your MIVS Studio Has Been Registered', 'studio_registered.txt', lambda studio: True)
+MIVSEmail(IndieStudio, 'Your MIVS Studio Has Been Registered', 'studio_registered.txt')
 MIVSEmail(IndieGame, 'Your MIVS Game Video Has Been Submitted', 'game_video_submitted.txt', lambda game: game.video_submitted)
 MIVSEmail(IndieGame, 'Your MIVS Game Has Been Submitted', 'game_submitted.txt', lambda game: game.submitted)
 
@@ -35,3 +38,5 @@ MIVSEmail(IndieGame, 'Your game application has been declined from MIVS', 'game_
 
 MIVSEmail(IndieGame, 'Your MIVS application has been waitlisted', 'game_waitlisted.txt',
           lambda game: game.status == c.WAITLISTED)
+
+MIVSEmail(IndieJudge, 'MIVS Judging is about to begin!', 'judge_intro.txt')
