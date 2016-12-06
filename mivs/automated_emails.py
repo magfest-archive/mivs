069dd474e2024960a2e8555049ed9403d097964b
LowPolyCount <LowPolyCount@users.noreply.github.com>
2016-12-06 08:31:41 -0500

    Adding MIVS email

--- mivs/automated_emails.py
+++ mivs/automated_emails.py
@@ -48,6 +48,9 @@ MIVSEmail(IndieGame, 'Your MIVS application has been waitlisted', 'game_waitlist
 MIVSEmail(IndieGame, 'Last chance to accept your MIVS booth', 'game_accept_reminder.txt',
           lambda game: game.status == c.ACCEPTED and not game.studio.group_id, when=days_before(2, c.MIVS_CONFIRM_DEADLINE))
 
+MIVSEmail(IndieGame, 'MIVS December Updates: Hotels and Magfest Versus!', 'december_updates.txt',
+          lambda game: game.status == c.ACCEPTED)
+
 MIVSEmail(IndieGame, 'MIVS judging is wrapping up', 'round_two_closing.txt',
           lambda game: game.submitted, when=days_before(14, c.JUDGING_DEADLINE))
 
