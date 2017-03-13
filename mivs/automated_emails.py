e023e97c4d1105de7bf6274127fbdeb324d0549d
Rob Ruana <rob@robruana.com>
2017-03-12 21:11:17 -0700

    Fixes #2383: Adds static ident arg to each AutomatedEmail instance

--- mivs/automated_emails.py
+++ mivs/automated_emails.py
@@ -18,56 +18,84 @@ class MIVSEmail(AutomatedEmail):
         AutomatedEmail.__init__(self, *args, sender=c.MIVS_EMAIL, **kwargs)
 
 
-MIVSEmail(IndieStudio, 'Your MIVS Studio Has Been Registered', 'studio_registered.txt')
-MIVSEmail(IndieGame, 'Your MIVS Game Video Has Been Submitted', 'game_video_submitted.txt', lambda game: game.video_submitted)
-MIVSEmail(IndieGame, 'Your MIVS Game Has Been Submitted', 'game_submitted.txt', lambda game: game.submitted)
+MIVSEmail(IndieStudio, 'Your MIVS Studio Has Been Registered', 'studio_registered.txt',
+          ident='mivs_studio_registered')
+
+MIVSEmail(IndieGame, 'Your MIVS Game Video Has Been Submitted', 'game_video_submitted.txt',
+          lambda game: game.video_submitted,
+          ident='mivs_video_submitted')
+
+MIVSEmail(IndieGame, 'Your MIVS Game Has Been Submitted', 'game_submitted.txt',
+          lambda game: game.submitted,
+          ident='mivs_game_submitted')
 
 MIVSEmail(IndieStudio, 'MIVS - Wat no video?', 'videoless_studio.txt',
           lambda studio: days_after(2, studio.registered)()
                      and not any(game.video_submitted for game in studio.games),
+          ident='mivs_missing_video_inquiry',
           when=days_before(7, c.ROUND_ONE_DEADLINE))
 
 MIVSEmail(IndieGame, 'Last chance to submit your game to MIVS', 'round_two_reminder.txt',
-          lambda game: game.status == c.JUDGING and not game.submitted, when=days_before(7, c.ROUND_TWO_DEADLINE))
+          lambda game: game.status == c.JUDGING and not game.submitted,
+          ident='mivs_game_submission_reminder',
+          when=days_before(7, c.ROUND_TWO_DEADLINE))
 
 MIVSEmail(IndieGame, 'Your game has made it into MIVS Round Two', 'video_accepted.txt',
-          lambda game: game.status == c.JUDGING)
+          lambda game: game.status == c.JUDGING,
+          ident='mivs_game_made_it_to_round_two')
 
 MIVSEmail(IndieGame, 'Your game has been declined from MIVS', 'video_declined.txt',
-          lambda game: game.status == c.VIDEO_DECLINED)
+          lambda game: game.status == c.VIDEO_DECLINED,
+          ident='mivs_video_declined')
 
 MIVSEmail(IndieGame, 'Your game has been accepted into MIVS', 'game_accepted.txt',
-          lambda game: game.status == c.ACCEPTED)
+          lambda game: game.status == c.ACCEPTED,
+          ident='mivs_game_accepted')
 
 MIVSEmail(IndieGame, 'Your game application has been declined from MIVS', 'game_declined.txt',
-          lambda game: game.status == c.GAME_DECLINED)
+          lambda game: game.status == c.GAME_DECLINED,
+          ident='mivs_game_declined')
 
 MIVSEmail(IndieGame, 'Your MIVS application has been waitlisted', 'game_waitlisted.txt',
-          lambda game: game.status == c.WAITLISTED)
+          lambda game: game.status == c.WAITLISTED,
+          ident='mivs_game_waitlisted')
 
 MIVSEmail(IndieGame, 'Last chance to accept your MIVS booth', 'game_accept_reminder.txt',
-          lambda game: game.status == c.ACCEPTED and not game.confirmed, when=days_before(2, c.MIVS_CONFIRM_DEADLINE))
+          lambda game: game.status == c.ACCEPTED and not game.confirmed, when=days_before(2, c.MIVS_CONFIRM_DEADLINE),
+          ident='mivs_accept_booth_reminder')
 
 MIVSEmail(IndieGame, 'MIVS December Updates: Hotels and Magfest Versus!', 'december_updates.txt',
-          lambda game: game.confirmed)
+          lambda game: game.confirmed,
+          ident='mivs_december_updates')
 
-MIVSEmail(IndieGame, 'REQUIRED: Pre-flight for MIVS due by midnight, January 2nd', 'game_preflight.txt', lambda game: game.confirmed)
+MIVSEmail(IndieGame, 'REQUIRED: Pre-flight for MIVS due by midnight, January 2nd', 'game_preflight.txt',
+          lambda game: game.confirmed,
+          ident='mivs_game_preflight_reminder')
 
 MIVSEmail(IndieGame, 'MIVS judging is wrapping up', 'round_two_closing.txt',
-          lambda game: game.submitted, when=days_before(14, c.JUDGING_DEADLINE))
+          lambda game: game.submitted, when=days_before(14, c.JUDGING_DEADLINE),
+          ident='mivs_round_two_closing')
 
-MIVSEmail(IndieJudge, 'MIVS Judging is about to begin!', 'judge_intro.txt')
+MIVSEmail(IndieJudge, 'MIVS Judging is about to begin!', 'judge_intro.txt',
+          ident='mivs_judge_intro')
 
-MIVSEmail(IndieJudge, 'MIVS Judging has begun!', 'judging_begun.txt')
+MIVSEmail(IndieJudge, 'MIVS Judging has begun!', 'judging_begun.txt',
+          ident='mivs_judging_has_begun')
 
 MIVSEmail(IndieJudge, 'MIVS Judging is almost over!', 'judging_reminder.txt',
-          when=days_before(7, c.SOFT_JUDGING_DEADLINE))
+          when=days_before(7, c.SOFT_JUDGING_DEADLINE),
+          ident='mivs_judging_due_reminder')
 
 MIVSEmail(IndieJudge, 'Reminder: MIVS Judging due by {}'.format(c.JUDGING_DEADLINE.strftime('%B %-d')), 'final_judging_reminder.txt',
-          lambda judge: not judge.judging_complete, when=days_before(5, c.JUDGING_DEADLINE))
+          lambda judge: not judge.judging_complete,
+          when=days_before(5, c.JUDGING_DEADLINE),
+          ident='mivs_judging_due_reminder_last_chance')
 
-MIVSEmail(IndieJudge, 'MIVS Judging and {EVENT_NAME} Staffing', 'judge_staffers.txt')
+MIVSEmail(IndieJudge, 'MIVS Judging and {EVENT_NAME} Staffing', 'judge_staffers.txt',
+          ident='mivs_judge_staffers')
 
-MIVSEmail(IndieJudge, 'MIVS Judge badge information', 'judge_badge_info.txt')
+MIVSEmail(IndieJudge, 'MIVS Judge badge information', 'judge_badge_info.txt',
+          ident='mivs_judge_badge_info')
 
-MIVSEmail(IndieJudge, 'MIVS Judging about to begin', 'judge_2016.txt')
+MIVSEmail(IndieJudge, 'MIVS Judging about to begin', 'judge_2016.txt',
+          ident='mivs_selected_to_judge')
